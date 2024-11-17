50ec85f215d7dca5b171cd760ee4cc2c51a3b2b2
Duarte Rodrigues <duartex.vdc@gmail.com>
2024-11-17 20:21:20 +0000

    FIle reader não terminado

--- src/pt/iscte/poo/game/Room.java
+++ src/pt/iscte/poo/game/Room.java
@@ -1,12 +1,20 @@
 package pt.iscte.poo.game;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import objects.Wall;
+import objects.Stair;
+import objects.Floor;
+
 import pt.iscte.poo.gui.ImageGUI;
+import pt.iscte.poo.gui.ImageTile;
 import pt.iscte.poo.utils.Direction;
 import pt.iscte.poo.utils.Point2D;
 
 public class Room {
 
-    private Point2D heroStartingPosition = new Point2D(9, 9);
+    private Point2D heroStartingPosition = new Point2D(9, 8);
     private JumpMan jumpMan;
 
     public Room() {
@@ -19,4 +27,33 @@ public class Room {
         jumpMan.move(direction);
         ImageGUI.getInstance().update(); // Atualiza a GUI com a nova posição
     }
+
+    private void fileReader(File file) { //Não terminado
+        int x = 0;
+        int y = 0;
+        ImageTile wall;
+        ImageTile stair;
+        try {
+            Scanner scan = new Scanner(file);
+            scan.nextLine();
+            while (scan.hasNextLine()) {
+                Point2D point = new Point2D(x, y);
+                switch(scan.next()) { //Não terminado
+                    case " ":
+                        continue;
+                    case "W":
+                        wall = new Wall(point);
+                    case "S":
+                        stair = new Stair(point);
+                    default:
+                        throw new IllegalArgumentException();
+                }
+                ImageGUI.getInstance().addImage(wall); // Não funciona
+            }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
+        }
+    }
+
 }
