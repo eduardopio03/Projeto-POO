e7f4f62489ae0e7dadf84159cb9b8e7efd2c247b
eduardopio03 <eduardojosepio@gmail.com>
2024-11-17 21:35:00 +0000

    GoodMeat

---
+++ src/pt/iscte/poo/game/GoodMeat.java
@@ -0,0 +1,29 @@
+package pt.iscte.poo.game;
+
+import pt.iscte.poo.utils.Point2D;
+
+public class GoodMeat extends GoodConsumable {
+
+    public GoodMeat(Point2D position) {
+        super(position);
+    }
+
+    @Override
+    public String getName() {
+        return "GoodMeat";
+    }
+
+    @Override
+    public int getLayer() {
+        return 1;
+    }
+
+    @Override
+    public void consume(JumpMan jumpMan) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'consume'");
+    }
+
+
+
+}
