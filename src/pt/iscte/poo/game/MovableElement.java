ef3cbe4b8af128eeac123f973fee111e894c6427
eduardopio03 <eduardojosepio@gmail.com>
2024-11-17 17:41:23 +0000

    Delete interface Movable

--- src/pt/iscte/poo/game/MovableElement.java
+++ src/pt/iscte/poo/game/MovableElement.java
@@ -4,20 +4,25 @@ import pt.iscte.poo.gui.ImageTile;
 import pt.iscte.poo.utils.Direction;
 import pt.iscte.poo.utils.Point2D;
 
-public abstract class MovableElement implements Movable, ImageTile {
-    private Point2D position; // Posição do elemento no jogo
-
+public abstract class MovableElement implements ImageTile {
+    private Point2D position;
 
     public MovableElement(Point2D startPosition) {
         this.position = startPosition;
     }
 
-    @Override
-    public void move(Direction direction) {
-        //TODO: Implementar o movimento do elemento
-    }
+    public abstract void move(Direction direction);
 
     public Point2D getPosition() {
         return position;
+    }
+
+    protected void setPosition(Point2D position) {
+        this.position = position; // Controla internamente a atualização da posição
+    }
+
+    @Override
+    public String getName() {
+        return getClass().getSimpleName();
     }
 }
