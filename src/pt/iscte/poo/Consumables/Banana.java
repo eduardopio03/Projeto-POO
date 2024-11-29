df33e9f1cb3ee44ea551200ba9d9f4fe7e37a368
Duarte Rodrigues <duartex.vdc@gmail.com>
2024-11-29 19:24:55 +0000

    Dano da banana

--- src/pt/iscte/poo/Consumables/Banana.java
+++ src/pt/iscte/poo/Consumables/Banana.java
@@ -1,18 +1,19 @@
 package pt.iscte.poo.Consumables;
 
 import pt.iscte.poo.Characters.JumpMan;
-import pt.iscte.poo.Characters.MapHandler;
+import pt.iscte.poo.game.Room;
 import pt.iscte.poo.gui.ImageGUI;
+import pt.iscte.poo.gui.ImageTile;
 import pt.iscte.poo.utils.Direction;
 import pt.iscte.poo.utils.Point2D;
 
 public class Banana extends BadConsumable {
 
-    private final MapHandler mapHandler;
+    private final Room room;
 
-    public Banana(Point2D position, MapHandler mapHandler) {
+    public Banana(Point2D position, Room room) {
         super(position);
-        this.mapHandler = mapHandler;
+        this.room = room;
     }
 
     @Override
@@ -23,6 +24,10 @@ public class Banana extends BadConsumable {
     @Override
     public int getLayer() {
         return 1;
+    }
+
+    public Room getRoom() {
+        return room;
     }
 
     @Override
@@ -36,13 +41,24 @@ public class Banana extends BadConsumable {
     Point2D nextPosition = getPosition().plus(direction.asVector());
 
     // Atualizar o tabuleiro antes de alterar a posição
-    if (mapHandler != null) { // Verificar se o MapHandler está configurado
-        mapHandler.updatePosition(getPosition(), nextPosition, this);
+    if (getRoom() != null) { // Verificar se o MapHandler está configurado
+        getRoom().updatePosition(getPosition(), nextPosition, this);
 
     }
 
     // Atualizar a posição da Banana
     setPosition(nextPosition);
+
+    if(getRoom().getBoardMap().get(nextPosition).get(0) instanceof JumpMan) {
+        ImageTile tile = getRoom().getBoardMap().get(nextPosition).get(0);
+        consume((JumpMan) tile);
+    }
+    if(getRoom().getBoardMap().get(nextPosition).size() > 1) {
+        if(getRoom().getBoardMap().get(nextPosition).get(1) instanceof JumpMan) {
+            ImageTile tile = getRoom().getBoardMap().get(nextPosition).get(1);
+            consume((JumpMan) tile);
+        }
+    }
 }
 
 
